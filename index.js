4b253acc03d4603536400af46b788e19347d50de
Manuel Comi <mcomi@hotmail.com>
2016-11-29 17:36:19 -0600

    add generic message

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/webhook/', function (req, res) {
       let sender = event.sender.id
       if (event.message && event.message.text) {
         let text = event.message.text
-        if (text === 'Generic') {
+        if (text === 'generic') {
             sendGenericMessage(sender)
             continue
         }
