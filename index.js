c4a0fe346b19bb1996c19b9bbef76d49791157ce
Manuel Comi <mcomi@hotmail.com>
2016-12-13 18:04:04 -0600

    change my functions

--- index.js
+++ index.js
@@ -1,136 +1,101 @@
-'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
 
-app.set('port', (process.env.PORT || 5000))
-
-//Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended:false}))
-
-//Process application/Json
-app.use(bodyParser.json())
-
-// Index route
+// Server frontpage
 app.get('/', function (req, res) {
-	res.send('Hola, soy un bot')
-})
+    res.send('This is TestBot Server');
+});
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
-
-// app.post('/webhook/', function (req, res) {
-// 	let messaging_events = req.body.entry[0].messaging
-// 	for (let i = 0; i < messaging_events.length; i++) {
-// 		let event = req.body.entry[0].messaging[i]
-// 		let sender = event.sender.id
-// 		if (event.message && event.message.text) {
-// 			let text = event.message.text
-// 			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-// 		}
-// 	}
-// 	res.sendStatus(200)
-// })
-
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
-      if (event.message && event.message.text) {
-        let text = event.message.text
-        if (text === 'generic') {
-            sendGenericMessage(sender)
-            continue
-        }
-        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-      }
-      if (event.postback) {
-        let text = JSON.stringify(event.postback)
-        sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-        continue
-      }
+// Facebook Webhook
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
     }
-    res.sendStatus(200)
-  })
+});
 
-//const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "EAAXB1rSjD4gBAPjlm0X8IGWO1NEXH89wC7VZBrbMAXPpSCyC9gpvlDzomYDqKVL0TePZACVcRYIbeJGb8EvZC8gZC23tKVVZCqNmcke6vBfcnm53JBboUK1Gb7NhC96JZAZBgVV11YdyNWgqZC9lHj7DFXVLdagqTgSWt9zcrvO9CQZDZD"
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            if (!kittenMessage(event.sender.id, event.message.text)) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
+        } else if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
+        }
+    }
+    res.sendStatus(200);
+});
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
+// generic function sending messages
+function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
-            recipient: {id:sender},
-            message: messageData,
+            recipient: {id: recipientId},
+            message: message,
         }
     }, function(error, response, body) {
         if (error) {
-            console.log('Error sending messages: ', error)
+            console.log('Error sending message: ', error);
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
+            console.log('Error: ', response.body.error);
         }
-    })
-}
+    });
+};
 
-function sendGenericMessage (sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type":"web_url",
-						"url": "https://www.twitter.com",
-						"title": "web url"
-					},{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "PAyload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type":"postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}, function (error, response, body) {
-			if (error) {
-				console.log('Error sending messages: ', error)
-			} else if (response.body.error) {
-				console.log('Error: ', response.body.error)
-			}
-		}
-	})
-}
-
-app.listen(app.get('port'), function () {
-	console.log('running on port', app.get('port'))
-})
+// send rich message with kitten
+function kittenMessage(recipientId, text) {
+    
+    text = text || "";
+    var values = text.split(' ');
+    
+    if (values.length === 3 && values[0] === 'kitten') {
+        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+            
+            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
+            
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, {
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+                    }
+                }
+            };
+    
+            sendMessage(recipientId, message);
+            
+            return true;
+        }
+    }
+    
+    return false;
+    
+};
