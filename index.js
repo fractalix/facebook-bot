8f99b34c0b75b868f301f148a84e2a507f5e521b
Manuel Comi <mcomi@hotmail.com>
2016-12-13 19:33:31 -0600

    get user first name typo

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ function getUserInfo(recipientId) {
         } else if (response.body.error) {
             console.log('Error: ', response.body.error);
         } else {
-        	return response.body.first_name
+        	var user = body.first_name
+        	return user
         }
     });
 }
